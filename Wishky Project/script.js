f44f582ca795e4aaa81816da3dc16f1aed5721e5
Shashthee Bisen <111593799+shashtheebisen@users.noreply.github.com>
2022-12-17 01:18:45 +0530

    Add files via upload

---
+++ Wishky Project/script.js
@@ -0,0 +1,165 @@
+
+var tl = gsap.timeline()
+tl.from("#page1>nav",{
+    y:-100,
+    duration:0.5,
+    // delay:0.5,
+    opacity:0,
+})
+tl.from(".main1>h1",{
+    y:600,
+    opacity:0,
+    duration:0.5,
+    // delay:0.1,
+})
+tl.from(".main1>h2",{
+    y:500,
+    opacity:0,
+    duration:0.5,
+    // delay:0.1,
+})
+tl.from(".main1>h3",{
+    y:400,
+    opacity:0,
+    duration:0.5,
+    // delay:0.1,
+})
+tl.from(".main1>h4",{
+    y:300,
+    opacity:0,
+    duration:0.5,
+    // delay:0.1,
+})
+tl.from(".main1>h5",{
+    y:200,
+    opacity:0,
+    duration:0.5,
+    // delay:0.1,
+})
+tl.from(".main2>img",{
+    y:600,
+    opacity:0,
+    duration:1,
+    // delay:0.5,
+})
+tl.from(".hello>h1",{
+    y:600,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".hello>h2",{
+    y:500,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".hello>h3",{
+    y:400,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".hello>h4",{
+    y:300,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".hello>h5",{
+    y:200,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".try1>h1",{
+    y:100,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".try1>h2",{
+    y:100,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from(".try1>button",{
+    y:100,
+    opacity:0,
+    // delay:0.1,
+    duration:0.5
+})
+tl.from("#try2>.img",{
+    y:300,
+    opacity:0,
+    // delay:0.5,
+    duration:0.5,
+})
+tl.from(".fry1>h1",{
+    x:-200,
+    opacity:0,
+    // delay:0.5,
+    duration:0.5,
+})
+tl.from(".fry1>h2",{
+    x:-200,
+    opacity:0,
+    // delay:0.5,
+    duration:0.5,
+})
+tl.from(".fry2>h1",{
+    y:100,
+    opacity:0,
+    duartion:0.5,
+})
+tl.from(".fry2>h2",{
+    y:100,
+    opacity:0,
+    duartion:0.5,
+})
+tl.from(".fry2>h3",{
+    y:100,
+    opacity:0,
+    duartion:0.5,
+})
+tl.from(".fry2>input",{
+    y:100,
+    opacity:0,
+    duartion:0.5,
+})
+tl.from("#meow",{
+    y:100,
+    opacity:0,
+    duartion:0.5, 
+})
+tl.from(".fry2>h4",{
+    y:100,
+    opacity:0,
+    duartion:0.5,
+})
+tl.from(".fry3>ul",{
+    x:100,
+    opacity:0,
+    duartion:0.5, 
+})
+tl.from("#back2>h1",{
+    x:100,
+    opacity:0,
+    duartion:0.5, 
+})
+tl.from("#back2>h2",{
+    x:100,
+    opacity:0,
+    duartion:0.5, 
+})
+tl.from("#back2>h3",{
+    x:100,
+    opacity:0,
+    duartion:0.5, 
+})
+tl.from("#back2>h4",{
+    x:100,
+    opacity:0,
+    duartion:0.5, 
+})
